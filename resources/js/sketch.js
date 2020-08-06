4c550c33e2c18a89e448325d2bd02933b0dd347c
gerizim16 <gerizim16@gmail.com>
2020-08-07 00:59:43 +0800

    Debug mode

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -8,26 +8,56 @@ function snake(sketch) {
         white: [220, 220, 220],
     }
 
-    function cameraOrbit(x, y, z) {
-        
+    function cameraOrbit(x, y, z, camera) {
+        // camera.camera(0, -30, 100);
+        camera.camera(y * 10, x * 10, 600, 0, 0, 0, 0, 1, 0);
+        if (debug) {
+            sketch.push();
+            sketch.fill(0);
+            sketch.text(`x: ${x}\ny: ${y}\nz: ${z}`, 0, -100);
+            sketch.pop();
+        }
     }
 
     const debug = true;
+    let camera;
+    let rotation;
+    let helvetica;
 
     sketch.preload = function () {
+        helvetica = sketch.loadFont('resources/fonts/Helvetica.ttf');
     };
 
     sketch.setup = function () {
-        let canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+        let canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight, sketch.WEBGL);
         canvas.style('display', 'block');
         // sketch.frameRate(30);
-        
+
+        rotation = {};
+
+        camera = sketch.createCamera();
+        sketch.setCamera(camera);
+
+        sketch.textFont(helvetica);
+        sketch.textSize(11);
+
         if (debug) {
             sketch.debugMode();
         }
     };
 
     sketch.draw = function () {
+        [rotation.z, rotation.x, rotation.y] = [sketch.rotationZ, sketch.rotationX, sketch.rotationY]
+
+        sketch.background(255);
+        cameraOrbit(rotation.x, rotation.y, rotation.z, camera);
+
+        sketch.fill(255, 104, 94);
+        sketch.box(85);
+
+        if (debug) {
+            // sketch.orbitControl();
+        }
     };
 
     sketch.mouseMoved = function () {
